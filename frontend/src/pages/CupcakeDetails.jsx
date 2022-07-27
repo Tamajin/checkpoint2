bfa4181913b0f0794da69aa7e602f0eb996d4917
Tamajin <wild.jlq@gmail.com>
2022-07-27 15:47:48 +0200

    fix errors prettier v2

--- frontend/src/pages/CupcakeDetails.jsx
+++ frontend/src/pages/CupcakeDetails.jsx
@@ -1,24 +1,18 @@
-import Cupcake from "@components/Cupcake";
 import axios from "axios";
-import React, { useEffect, useState } from "react"
-import { useParams } from "react-router-dom"
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import Cupcake from "../components/Cupcake";
 
-export default function CupcakeDetails () {
-    const { id } = useParams();
-    const [ details, setDetails ] = useState( [] )
+export default function CupcakeDetails() {
+  const { id } = useParams();
+  const [details, setDetails] = useState([]);
 
-    useEffect(() => {
-        axios
-        .get(`http://localhost:4000/cupcakes/${id}`)
-        .then(response => response.data)
-        .then(data => setDetails(data))
-      }, []);
-    
+  useEffect(() => {
+    axios
+      .get(`http://localhost:4000/cupcakes/${id}`)
+      .then((response) => response.data)
+      .then((data) => setDetails(data));
+  }, []);
 
-  return (
-    <>
-    <Cupcake cupcake={details}/>
-    </>
-  )
+  return <Cupcake cupcake={details} />;
 }
-
--- frontend/src/pages/CupcakeList.jsx
+++ frontend/src/pages/CupcakeList.jsx
@@ -1,7 +1,7 @@
-import React, {useState, useEffect} from "react";
-import Cupcake from "@components/Cupcake";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
+import Cupcake from "../components/Cupcake";
 
 export default function CupcakeList() {
   // Step 1: get all cupcakes
@@ -10,11 +10,10 @@ export default function CupcakeList() {
 
   useEffect(() => {
     axios
-    .get("http://localhost:4000/cupcakes")
-    .then(response => response.data)
-    .then(data => setCupcakes(data))
+      .get("http://localhost:4000/cupcakes")
+      .then((response) => response.data)
+      .then((data) => setCupcakes(data));
   }, []);
-
 
   // Step 3: get all accessories
 
@@ -22,9 +21,9 @@ export default function CupcakeList() {
 
   useEffect(() => {
     axios
-    .get("http://localhost:4000/accessories")
-    .then(response => response.data)
-    .then(data => setAccessories(data))  
+      .get("http://localhost:4000/accessories")
+      .then((response) => response.data)
+      .then((data) => setAccessories(data));
   }, []);
 
   const [selectedAccessory, setSelectedAccessory] = useState("");
@@ -32,7 +31,7 @@ export default function CupcakeList() {
   const handleChangeSelectedAccessory = (e) => {
     e.preventDefault();
     setSelectedAccessory(e.target.value);
-  }
+  };
 
   return (
     <>
@@ -40,39 +39,36 @@ export default function CupcakeList() {
       <form className="center">
         <label htmlFor="cupcake-select">
           Filter by{" "}
-          <select 
-          id="cupcake-select"
-          onChange={handleChangeSelectedAccessory}
-          >
+          <select id="cupcake-select" onChange={handleChangeSelectedAccessory}>
             <option value="">---</option>
             {/* Step 4: add an option for each accessory */}
-            {accessories && accessories
-            .map((accessorie)=>(
-              <option 
-              value={accessorie.id} 
-              key={` key + ${accessorie.id}`}
-              >{accessorie.name}
-              </option>
-            ))}
+            {accessories &&
+              accessories.map((accessorie) => (
+                <option value={accessorie.id} key={` key + ${accessorie.id}`}>
+                  {accessorie.name}
+                </option>
+              ))}
           </select>
         </label>
       </form>
       <ul className="cupcake-list" id="cupcake-list">
         {/* Step 2: repeat this block for each cupcake */}
-        
-        {cupcakes && cupcakes
-        .filter((cupcake) => !selectedAccessory || cupcake.accessory_id === selectedAccessory)
-        .map((cupcake)=> 
-        <li className="cupcake-item">
-        <Link to={`/cupcakes/${cupcake.id}`}>
-        <Cupcake 
-        cupcake={cupcake} 
-        key={cupcake.id}
-        />
-        </Link>
-        </li>)}          
-        
-        {/* end of block */} 
+
+        {cupcakes &&
+          cupcakes
+            .filter(
+              (cupcake) =>
+                !selectedAccessory || cupcake.accessory_id === selectedAccessory
+            )
+            .map((cupcake) => (
+              <li className="cupcake-item">
+                <Link to={`/cupcakes/${cupcake.id}`}>
+                  <Cupcake cupcake={cupcake} key={cupcake.id} />
+                </Link>
+              </li>
+            ))}
+
+        {/* end of block */}
       </ul>
     </>
   );
