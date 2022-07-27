037cd3a86b6e3baa2eecc344bc9f671cdb31dcac
Tamajin <wild.jlq@gmail.com>
2022-07-27 10:06:52 +0200

    fecth data and display in cupckae component via map and props

--- frontend/src/pages/CupcakeList.jsx
+++ frontend/src/pages/CupcakeList.jsx
@@ -1,7 +1,19 @@
+import React, {useState, useEffect} from "react";
 import Cupcake from "@components/Cupcake";
+import axios from "axios";
 
 export default function CupcakeList() {
   // Step 1: get all cupcakes
+
+  const [cupcakes, setCupcakes] = useState([]);
+
+  useEffect(() => {
+    axios
+    .get("http://localhost:4000/cupcakes")
+    .then(response => response.data)
+    .then(data => setCupcakes(data))
+  }, []);
+
 
   // Step 3: get all accessories
 
@@ -19,10 +31,12 @@ export default function CupcakeList() {
       </form>
       <ul className="cupcake-list" id="cupcake-list">
         {/* Step 2: repeat this block for each cupcake */}
+        {cupcakes && cupcakes
+        .map((cupcake)=> <Cupcake cupcake={cupcake}/>)}
         <li className="cupcake-item">
-          <Cupcake />
+          
         </li>
-        {/* end of block */}
+        {/* end of block */} 
       </ul>
     </>
   );
